dda59e3651a8a57a20e1d47e38732b85b33fce7f
Raj Ashish Desai <rajdesai94@gmail.com>
2024-05-22 21:08:23 -0400

    update links

--- app/components/Links.tsx
+++ app/components/Links.tsx
@@ -6,11 +6,11 @@ const linkClass = 'bg-white p-1 rounded';
 const Links = () => {
   return (
     <div className='flex justify-between overflow-scroll gap-1'>
-      <Link className={linkClass} to='/breath/i4e4'>
-        Breath/i4e4
+      <Link className={linkClass} to='/breath/i3r3e3s3'>
+        Breath/i3r3e3s3
       </Link>
-      <Link className={linkClass} to='/breath/i5e6'>
-        Breath/i5e6
+      <Link className={linkClass} to='/breath/i6r4e6s4'>
+        Breath/i6r4e6s4
       </Link>
       <Link className={linkClass} to='/breath/i5r5e5s5'>
         Breath/i5r5e5s5
