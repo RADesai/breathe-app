25051c45cb3aa7f967e0c564c5a6dce9a02ccf64
Raj Ashish Desai <rajdesai94@gmail.com>
2024-12-29 22:19:44 -0500

    rm remix deps

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
-import { netlifyPlugin } from '@netlify/remix-adapter/plugin';
 import { reactRouter } from '@react-router/dev/vite';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [reactRouter(), netlifyPlugin(), tsconfigPaths()]
+  plugins: [reactRouter(), tsconfigPaths()]
 });
