98bb116979a705e85006ad5be66fb254dd505891
Raj Ashish Desai <rajdesai94@gmail.com>
2024-12-29 22:24:45 -0500

    add rr netlify plugin

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,13 @@ import { reactRouter } from '@react-router/dev/vite';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
+import netlifyPlugin from '@netlify/vite-plugin-react-router';
+
+
 export default defineConfig({
-  plugins: [reactRouter(), tsconfigPaths()]
+  plugins: [
+    reactRouter(),
+    tsconfigPaths(),
+    netlifyPlugin()
+  ]
 });
