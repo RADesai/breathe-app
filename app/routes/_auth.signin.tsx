07882cd064dd83e08f9463b8b9bb7c5661bdb8fb
Raj Ashish Desai <rajdesai94@gmail.com>
2025-01-27 18:58:29 -0500

    show spinner and redirect from login if already signed in

--- app/routes/_auth.signin.tsx
+++ app/routes/_auth.signin.tsx
@@ -3,6 +3,7 @@ import { Session } from "@supabase/supabase-js";
 import { useEffect } from "react";
 import { data as rrData, useActionData, useNavigate } from "react-router";
 import AuthForm from "~/components/auth/AuthForm";
+import Spinner from "~/components/Spinner";
 import { useSession } from "~/context/SessionProvider";
 import { sessionCookie } from "~/db/cookies";
 import { getSupabaseServer } from "~/db/supabaseServer";
@@ -23,7 +24,6 @@ export async function action({ request }: Route.ActionArgs) {
   }
 
   console.log("Sign-in action complete:", data);
-
   return rrData(
     { session: data.session },
     {
@@ -37,24 +37,22 @@ export async function action({ request }: Route.ActionArgs) {
 export default function SignIn() {
   const { session, syncSession } = useSession();
   const navigate = useNavigate();
-
   const actionData = useActionData<{ session?: Session }>();
-  console.log("sign in actionData:", actionData);
 
   useEffect(() => {
     if (
       actionData?.session &&
       actionData?.session.user?.id !== session?.user?.id
-    ) {
-      console.log(
-        "<SignIn.useEffect> actionData?.session.user?.id !== session?.user?.id",
-        actionData?.session.user?.id,
-        session?.user?.id,
-      );
+    ) { // use actionData to sync session if it exists
       syncSession(actionData.session);
       navigate("/", { replace: true });
+    } else if (session) {
+      console.error("Session Already Found, redirecting to /");
+      navigate("/", { replace: true });
     }
-  }, [actionData?.session, navigate, session?.user?.id, syncSession]);
+  }, [actionData, navigate, session, syncSession]);
+
+  if (session) return <Spinner />;
 
   return <AuthForm actionUrl="/signin" buttonText="Sign In" />;
 }
